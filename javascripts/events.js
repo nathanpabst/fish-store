4f399670f4e30390bb8c13629835f1c2b11cfcd5
Nathan Pabst <nathan.pabst@gmail.com>
2018-05-01 21:33:53 -0500

    completed project

--- javascripts/events.js
+++ javascripts/events.js
@@ -19,19 +19,18 @@ const changeButtonText = () => {
 const moveToCart = (e) => {
   const fishCard = $(e.target).closest('.fish');
   $('#snagged').append(fishCard);
-  $(e.target).text('Remove from Cart');
-  $(e.target).on('click', removeFromCart);
+  $(e.target).text('Remove from Cart').removeClass('add btn-danger').addClass('remove btn-info');
 };
 
 const removeFromCart = (e) => {
   const fishCard = $(e.target).closest('.fish');
   $('#available').append(fishCard);
-  $(e.target).text('Add to Cart');
-  $(e.target).on('click', moveToCart);
+  $(e.target).text('Add to Basket').removeClass('remove btn-info').addClass('add btn-danger');
 };
 
 const bindEvents = () => {
-  $('button.add').on('click', moveToCart);
+  $('body').on('click', '.remove', removeFromCart);
+  $('body').on('click', '.add', moveToCart);
   $('#show-sale').click(() => {
     changeButtonText();
     filterFish();
